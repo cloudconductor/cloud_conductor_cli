e665b14e33fa6d866debdcd11541ef2a80e98297
bsmile <battlesmile@gmail.com>
2016-03-03 14:09:00 +0900

    Modify token of rubocop and cli command message

--- lib/cloud_conductor_cli/models/token.rb
+++ lib/cloud_conductor_cli/models/token.rb
@@ -6,12 +6,12 @@ module CloudConductorCli
       include Models::Base
 
       desc 'get', 'Get token'
-      method_option :email, type: :string, required: false, default: ENV['CC_AUTH_ID'], desc: 'Account email'
-      method_option :password, type: :string, required: false,default: ENV['CC_AUTH_PASSWORD'],desc: 'Account password'
+      method_option :email, type: :string, required: false, default: ENV['CC_AUTH_ID'], desc: 'Account email. use CC_AUTH_ID environment if not specified.'
+      method_option :password, type: :string, required: false, default: ENV['CC_AUTH_PASSWORD'], desc: 'Account password. use CC_AUTH_PASSWORD environment if not specified.'
 
       def get
         payload = declared(options, self.class, :get)
-        response = connection.post("/tokens", payload)
+        response = connection.post('/tokens', payload)
         output(response)
       end
     end
