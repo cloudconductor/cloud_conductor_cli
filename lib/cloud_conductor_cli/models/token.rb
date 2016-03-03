e0fee1a063385f07df510dbe9a63a06f7d6f93ab
bsmile <battlesmile@gmail.com>
2016-03-03 15:21:31 +0900

    Delete default message email and password from cc-cli token get command

--- lib/cloud_conductor_cli/models/token.rb
+++ lib/cloud_conductor_cli/models/token.rb
@@ -6,11 +6,13 @@ module CloudConductorCli
       include Models::Base
 
       desc 'get', 'Get token'
-      method_option :email, type: :string, required: false, default: ENV['CC_AUTH_ID'], desc: 'Account email. use CC_AUTH_ID environment if not specified.'
-      method_option :password, type: :string, required: false, default: ENV['CC_AUTH_PASSWORD'], desc: 'Account password. use CC_AUTH_PASSWORD environment if not specified.'
+      method_option :email, type: :string, desc: 'Account email. use CC_AUTH_ID environment if not specified.'
+      method_option :password, type: :string, desc: 'Account password. use CC_AUTH_PASSWORD environment if not specified.'
 
       def get
         payload = declared(options, self.class, :get)
+        payload = payload.merge('email' => ENV['CC_AUTH_ID'])
+        payload = payload.merge('password' => ENV['CC_AUTH_PASSWORD'])
         response = connection.post('/tokens', payload)
         output(response)
       end
